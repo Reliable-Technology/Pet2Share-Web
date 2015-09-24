a75692f2cec19001b32d5084500023d51d3cf07f
shishir333 <shishir333@gmail.com>
2015-09-24 15:20:08 -0700

    The server repository v1 live on sept 24

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pet2Share_API.DAL;
+using Pet2Share_Web.Utility;
+
+namespace Pet2Share_Web.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var obj = Pet2Share_API.Service.AccountManagement.Login(model.Username, Utils.CStrDef(model.Password));
+                if (obj != null)
+                {
+                    return RedirectToLocal(returnUrl);
+                }
+            }
+            return View();
+        }
+
+        #region Helpers
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                //Redirect to HomePage
+                return RedirectToAction("Index", "Index");
+            }
+        }
+        #endregion
+
+    }
+}
